afbcab207c1ea8c37f7e53a8ffa761a5ce090f46
EchoHackCmd <EchoHackCmd@gmail.com>
2021-05-10 01:56:54 +0100

    Project

---
+++ MC Win10 Test/Utils.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <Windows.h>
+#include <Psapi.h>
+
+#include <vector>
+#include <string>
+
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <chrono>
+
+#include <map>
+
+#include <MinHook.h>
+
+#define INRANGE(x,a,b)   (x >= a && x <= b)
+#define GET_BYTE( x )    (GET_BITS(x[0]) << 4 | GET_BITS(x[1]))
+#define GET_BITS( x )    (INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
+
+struct Vec2 {
+	union {
+		struct {
+			float x, y;
+		};
+		float arr[2];
+	};
+	Vec2() { x = y = 0; };
+	Vec2(float x = 0, float y = 0) { this->x = x; this->y = y; };
+};
+
+struct Vec3 {
+	union {
+		struct {
+			float x, y, z;
+		};
+		float arr[3];
+	};
+	Vec3() { x = y = 0; };
+	Vec3(float x = 0, float y = 0, float z = 0) { this->x = x; this->y = y; this->z = z; };
+};
+
+class Utils {
+public:
+	static HMODULE hModule;
+	static bool hasExtension(std::string fileName);
+	static bool doesPathExist(std::string);
+	static void CreateDir(std::string);
+	static void DeletePath(std::string);
+	static void WriteToFile(std::string, std::string);
+	static void DebugLogOutput(std::string);
+	static uintptr_t FindAddr(uintptr_t, std::vector<unsigned int>);
+	static uintptr_t FindSig(const char*);
+	static std::string addrToStr(uintptr_t);
+};
---
+++ MC Win10 Test/dllmain.cpp
@@ -0,0 +1,72 @@
+#include "Utils.h"
+
+class ClientInstance {
+public:
+    class Player* GetLocalPlayer() {
+        static unsigned offset = 0;
+        if (offset == NULL) {
+            offset = *reinterpret_cast<int*>(Utils::FindSig("48 8B 89 ? ? ? ? 48 85 C9 ? ? 33 C0") + 3);
+        }
+        return *reinterpret_cast<Player**>((uintptr_t)(this) + offset);
+    }
+};
+
+class Player {
+public:
+    virtual void Function0();
+    virtual void Function1();
+    virtual void Function2();
+    virtual void Function3();
+    virtual void Function4();
+    virtual void Function5();
+    virtual void Function6();
+    virtual void Function7();
+    virtual void remove(void);
+    virtual void Function9();
+    virtual void Function10();
+    virtual void Function11();
+    virtual void setPos(Vec3 const&);
+};
+
+typedef void(__thiscall* CInstance)(ClientInstance*, void*);
+CInstance _CInstance;
+
+bool once = false;
+
+void CInstance_Callback(ClientInstance* cInstance, void* a2) {
+    if (!once) {
+        Utils::DebugLogOutput("Hooked!");
+        Player* player = cInstance->GetLocalPlayer();
+        if (player != nullptr) {
+            Vec3 tpPos = Vec3(0, 50, 0);
+            player->setPos(tpPos);
+        }
+        once = true;
+    }
+    _CInstance(cInstance, a2);
+}
+
+void initDLL(LPVOID lpParam) {
+    Utils::DeletePath("Client");
+
+    uintptr_t cInstance_addr = Utils::FindSig("48 8B 81 ? ? ? ? C3 CC CC CC CC CC CC CC CC 40 55");
+
+    if (MH_Initialize() == MH_OK) { /* Only initialize once */
+        if (MH_CreateHook((void*)cInstance_addr, &CInstance_Callback, reinterpret_cast<LPVOID*>(&_CInstance)) == MH_OK){
+            MH_EnableHook((void*)cInstance_addr);
+        }
+    }
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)initDLL, hModule, 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
